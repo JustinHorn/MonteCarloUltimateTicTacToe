6650897ca86d3fb0cecdaf1eefaace7be047cf05
y0092888 <ju-horn@web.de>
2019-06-10 17:38:16 +0200

    update

---
+++ src/tree/Test_TreeUtils.java
@@ -0,0 +1,148 @@
+package tree;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import org.junit.jupiter.api.Test;
+
+import Board.Board;
+import monte_carlo.MonteCarlo;
+import monte_carlo.Move;
+import monte_carlo.Utils;
+
+public class Test_TreeUtils {
+	
+	@Test
+	void test_getParentsFromNode() {
+		Root root = new Root();
+		Board b = new Board();
+		root.setChildren(Utils.getAvailableMoves(b));
+		Node random_child = root.getRandomChild();
+		ArrayList<Root> parents = TreeUtils.getParentsFromNode(random_child);
+		
+		assertEquals(root,parents.get(0));
+		assertEquals(1,parents.size());
+	}
+	
+	@Test
+	void test_getParentsFromNode_exceptRoot() {
+		Root root = new Root();
+		Board b = new Board();
+		
+		root.setChildren(Utils.getAvailableMoves(b));
+		Node random_child = root.getRandomChild();
+		b.makeMove(random_child.getMove());
+		random_child.setChildren(Utils.getAvailableMoves(b));
+		Node childOf_randomChild = random_child.getRandomChild();
+		
+		
+		ArrayList<Node> parent = TreeUtils.getParentsFromNode_exceptRoot(childOf_randomChild);
+		
+		assertEquals(random_child,parent.get(0));
+		assertEquals(1,parent.size());
+	}
+	
+	@Test
+	void test_getMovesFromParents() {
+		Root root = new Root();
+		Board b = new Board();
+		
+		root.setChildren(Utils.getAvailableMoves(b));
+		Node child1 = root.getRandomChild();
+		b.makeMove(child1.getMove());
+		child1.setChildren(Utils.getAvailableMoves(b));
+		Node child2 = child1.getRandomChild();
+		b.makeMove(child2.getMove());
+		child2.setChildren(Utils.getAvailableMoves(b));
+		Node child3 = child2.getRandomChild();
+		
+		Move[] moves = TreeUtils.getMoves_from_nodeParents_asArray(child3);
+		
+		assertEquals(child2.getMove(),moves[0]);
+		assertEquals(child1.getMove(),moves[1]);
+		
+		ArrayList<Move> moves2 = TreeUtils.getMoves_from_nodeParents_asList(child3);
+		Collections.reverse(moves2);
+		
+		assertEquals(child2.getMove(),moves2.get(1));
+		assertEquals(child1.getMove(),moves2.get(0));
+	}
+	
+
+	@Test
+	void test_getBestChild_and_mostSimulations() {
+		Board b = new Board();
+		Root root = new Root();
+		
+		root.setChildren(Utils.getAvailableMoves(b));
+		Node random_child = root.getRandomChild();
+		random_child.changeScore_and_simulationCount_of_MeAndParent(b.X);
+
+		assertEquals(random_child,TreeUtils.getChild_with_highestValue(root));
+		assertEquals(random_child,TreeUtils.getChild_with_mostSimulations(root));
+		
+		random_child.changeScore_and_simulationCount_of_MeAndParent(b.O);
+		assertTrue(random_child != TreeUtils.getChild_with_highestValue(root));
+		assertEquals(random_child,TreeUtils.getChild_with_mostSimulations(root));
+		
+	}
+	
+
+	@Test
+	void test_Node_thistoTree() {
+		Move lvl1 = new Move(1,2,3);
+		Move lvl2 = new Move(1,2,4);
+		Root root = setUptree_with_hightOfTwo(lvl1,lvl2);
+		
+		assertEquals(true,root.hasChild_withMove(lvl1));
+		Node child1 = root.getChild_withMove(lvl1);
+		
+		
+		assertEquals(lvl1,child1.getMove());
+		assertEquals(true,child1.hasChild_withMove(lvl2));
+
+		root = TreeUtils.node_to_Root(child1);
+		
+		assertEquals(true,root.hasChild_withMove(lvl2));
+
+	}
+	
+	private Root setUptree_with_hightOfTwo(Move m1,Move m2) {
+		Root root = new Root();
+		root.setChildren(new Move[]{m1});
+		Node randomChild = root.getRandomChild();
+		randomChild.setChildren(new Move[] {m2});
+		return root;
+	}
+	
+	@Test
+	void test_expandTree() {
+		Move lvl1 = new Move(1,2,3);
+		Move lvl2 = new Move(1,2,4);
+		Root root = setUptree_with_hightOfTwo(lvl1,lvl2);
+		
+		assertEquals(true,root.hasChild_withMove(lvl1));
+
+		root = TreeUtils.expandTree(root,lvl1);
+		assertEquals(true,root.hasChild_withMove(lvl2));
+	}
+	
+	@Test
+	void test_incorrect_TreeDoesNotMacthBoard() {
+		Board b = new Board();
+		Root root = new Root();
+		root.setChildren(Utils.getAvailableMoves(b));
+		b.makeMove(new Move(0,0,17));
+		
+		try {
+			MonteCarlo.calcMove(b,root,1);
+			fail("Made move without error");
+		} catch (Exception e) {
+
+		}
+	}
+}
---
+++ src/tree/TreeUtils.java
@@ -0,0 +1,98 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import Board.Board;
+import monte_carlo.Move;
+
+public class TreeUtils {
+
+	public static Root expandTree(Root root, Move m) {
+		if(root.hasChild_withMove(m)) {
+			Node child = root.getChild_withMove(m);
+			return node_to_Root(child);
+		} else {
+			throw new IllegalArgumentException("No such child: "+m);
+		}
+	}
+	
+	public static Root node_to_Root(Node n) {
+		Root root = new Root(n.getSimulationCount());
+		if(n.childrenAssigned()) {
+			root.setChildren(n.getChildren());
+		}
+		return root;
+		
+	}
+	
+	public static void doesTreeMatchBoard(Root root, Board board) {
+		ArrayList<Move> moves = board.getAvailableMoves();
+		if(moves.parallelStream().anyMatch(move -> !root.hasChild_withMove(move))) {
+			throw new IllegalArgumentException("First generation of tree Moves does not match avaialble moves of board");
+		}
+	}
+	
+	public static Node expandTree(Root root) {
+		Node bestChild = TreeUtils.getChild_with_highestValue(root);
+		while (bestChild.hasChildren()) {
+			bestChild = TreeUtils.getChild_with_highestValue(bestChild);
+		}
+		return bestChild;
+	}
+	
+	public static Node getChild_with_highestValue(Root root) {
+		Node[] child = root.getChildren();
+		if(child == null) {
+			throw new NullPointerException("Children have not been assinged!");
+		}
+		if(child.length > 0) {
+			long time = System.currentTimeMillis();
+			for(int i = 0; i < child.length;i++) {
+				child[i].calcValue(time);
+			}
+			Arrays.sort(child, new CompareValue());
+			return child[child.length-1];
+		} else {
+			return (Node) root;
+		}
+	}
+	
+	public static Node getChild_with_mostSimulations(Root root) {
+		if (root.childrenAssigned()) {
+				Node[] child = root.getChildren();
+				Arrays.sort(child, new CompareSimulationCount());
+				return child[child.length-1];
+		} 
+		throw new IllegalAccessError("Children have not been assinged!");
+	}
+	
+	public static ArrayList<Root> getParentsFromNode(Node n) {
+		ArrayList<Root> a = new ArrayList<Root>();
+		Root parent = n.getParent();
+		while(parent instanceof Node) {
+			a.add(parent);
+			parent = ((Node) parent).getParent();
+		}
+		a .add(parent);
+		return a;
+	}
+	
+	public static ArrayList<Node> getParentsFromNode_exceptRoot(Node n) {
+		ArrayList<Root> list = getParentsFromNode(n);
+		list.remove(list.size()-1);
+		return list.parallelStream().map(a -> (Node) a).collect(Collectors.toCollection(ArrayList<Node>::new));
+	}
+	
+	public static ArrayList<Move> getMoves_from_nodeParents_asList(Node n) {
+		ArrayList<Node> list =  getParentsFromNode_exceptRoot(n);
+		ArrayList<Move> moves = list.parallelStream().map(m_n-> m_n.getMove()).collect(Collectors.toCollection(ArrayList<Move>::new));
+		return moves;
+	}
+	
+	public static Move[] getMoves_from_nodeParents_asArray(Node n) {
+		ArrayList<Move> moves = getMoves_from_nodeParents_asList(n);
+		return moves.toArray(new Move[moves.size()]);
+	}
+}
